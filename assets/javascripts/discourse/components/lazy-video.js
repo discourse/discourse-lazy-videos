7d695e215dbc889e7fc36d43ac26895943fac2f7
Jan Cernik <jancernik12@gmail.com>
2023-02-13 22:01:17 -0300

    DEV: Refactor to use Glimmer Components Since now is possible to render Glimmer components inside posts,  t was a good opportunity to use that new functionality in this plugin.

---
+++ assets/javascripts/discourse/components/lazy-video.js
@@ -0,0 +1,21 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+
+export default class LazyVideo extends Component {
+  @tracked isLoaded = false;
+
+  @action
+  loadEmbed() {
+    this.isLoaded = true;
+    this.args.callback();
+  }
+
+  @action
+  onKeyPress(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      this.loadEmbed();
+    }
+  }
+}
--- assets/javascripts/initializers/lazy-videos.js
+++ assets/javascripts/initializers/lazy-videos.js
@@ -1,16 +1,39 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
+import getLazyAttributes from "../lib/data-extractor";
+import { hbs } from "ember-cli-htmlbars";
 
 function initLazyEmbed(api) {
-  const lazyVideos = api.container.lookup("service:lazy-videos");
-
   api.decorateCookedElement(
-    (cooked) => {
+    (cooked, helper) => {
+      const lazyContainers = cooked.querySelectorAll(".lazy-video-container");
+
       if (cooked.classList.contains("d-editor-preview")) {
         return;
       }
-      lazyVideos.decorateLazyContainers(cooked, api);
+
+      if (lazyContainers.length === 0) {
+        return;
+      }
+
+      for (const container of lazyContainers) {
+        const callback = () => {
+          const postId = cooked.closest("article").dataset.postId;
+          if (postId) {
+            api.preventCloak(parseInt(postId, 10));
+          }
+        };
+
+        const videoAttributes = getLazyAttributes(container);
+        const lazyVideo = helper.renderGlimmer(
+          "p.lazy-video-wrapper",
+          hbs`<LazyVideo @videoAttributes={{@data.param}} @callback={{@data.callback}}/>`,
+          { param: videoAttributes, callback }
+        );
+
+        container.replaceWith(lazyVideo);
+      }
     },
-    { id: "discourse-lazy-videos" }
+    { onlyStream: true, id: "discourse-lazy-videos" }
   );
 }
 
@@ -18,6 +41,6 @@ export default {
   name: "discourse-lazy-videos",
 
   initialize() {
-    withPluginApi("1.5.0", initLazyEmbed);
+    withPluginApi("1.6.0", initLazyEmbed);
   },
 };
